d9ed8639584e1b646ebabc123c4318a1047898e1
Jennifer Power <barnabei.jennifer@gmail.com>
2023-10-09 16:45:27 -0400

    chore: fixes spelling error on csv_transformer.py comment
    Signed-off-by: Jennifer Power <barnabei.jennifer@gmail.com>

--- trestlebot/transformers/csv_transformer.py
+++ trestlebot/transformers/csv_transformer.py
@@ -14,7 +14,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-"""CSV Tranformer for rule authoring."""
+"""CSV Transformer for rule authoring."""
 
 import csv
 import json
