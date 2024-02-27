df21e7fe65ff53c9e3c1d83d2fc0829ea9765d94
Jennifer Power <barnabei.jennifer@gmail.com>
2024-02-27 17:40:44 -0500

    refactor: adds a E2ETestRunner class to encapsulate logic for running E2E tests
    Signed-off-by: Jennifer Power <barnabei.jennifer@gmail.com>

--- tests/e2e/test_e2e_compdef.py
+++ tests/e2e/test_e2e_compdef.py
@@ -6,8 +6,7 @@
 
 import logging
 import pathlib
-import subprocess
-from typing import Dict, Tuple
+from typing import Dict, List, Tuple
 
 import pytest
 from git.repo import Repo
@@ -16,12 +15,8 @@ from trestle.core.models.file_content_type import FileContentType
 from trestle.oscal.component import ComponentDefinition
 from trestle.oscal.profile import Profile
 
-from tests.testutils import (
-    build_test_command,
-    load_from_json,
-    setup_for_profile,
-    setup_rules_view,
-)
+from tests.e2e.e2e_testutils import E2ETestRunner
+from tests.testutils import load_from_json, setup_for_profile, setup_rules_view
 from trestlebot.const import (
     ERROR_EXIT_CODE,
     INVALID_ARGS_EXIT_CODE,
@@ -75,48 +70,38 @@ test_comp_name = "test_comp"
 )
 def test_rules_transform_e2e(
     tmp_repo: Tuple[str, Repo],
-    podman_setup: Tuple[int, str],
+    e2e_runner: E2ETestRunner,
     test_name: str,
     command_args: Dict[str, str],
     response: int,
 ) -> None:
     """Test the trestlebot rules transform command."""
-    # Check that the container image was built successfully
-    # and the mock server is running
-    exit_code, image_name = podman_setup
-    assert exit_code == 0
-
     logger.info(f"Running test: {test_name}")
 
-    tmp_repo_str, repo = tmp_repo
+    tmp_repo_str, _ = tmp_repo
     tmp_repo_path = pathlib.Path(tmp_repo_str)
 
     # Setup the rules directory
     setup_rules_view(tmp_repo_path, test_comp_name)
 
-    command = build_test_command(
-        tmp_repo_str, "rules-transform", command_args, image_name
+    command: List[str] = e2e_runner.build_test_command(
+        tmp_repo_str, "rules-transform", command_args
     )
-    run_response = subprocess.run(command, capture_output=True)
-    assert run_response.returncode == response
+    exit_code, response_stdout = e2e_runner.invoke_command(command)
+    assert exit_code == response
 
     # Check that the component definition was created
-    if response == SUCCESS_EXIT_CODE:
+    if exit_code == SUCCESS_EXIT_CODE:
         if "skip-items" in command_args:
-            assert f"input: {test_comp_name}.csv" not in run_response.stdout.decode(
-                "utf-8"
-            )
+            assert f"input: {test_comp_name}.csv" not in response_stdout
         else:
             comp_path: pathlib.Path = ModelUtils.get_model_path_for_name_and_class(
                 tmp_repo_path, test_comp_name, ComponentDefinition, FileContentType.JSON
             )
             assert comp_path.exists()
-            assert f"input: {test_comp_name}.csv" in run_response.stdout.decode("utf-8")
+            assert f"input: {test_comp_name}.csv" in response_stdout
         branch = command_args["branch"]
-        assert (
-            f"Changes pushed to {branch} successfully."
-            in run_response.stdout.decode("utf-8")
-        )
+        assert f"Changes pushed to {branch} successfully." in response_stdout
 
 
 @pytest.mark.slow
@@ -198,17 +183,12 @@ def test_rules_transform_e2e(
 )
 def test_create_cd_e2e(
     tmp_repo: Tuple[str, Repo],
-    podman_setup: Tuple[int, str],
+    e2e_runner: E2ETestRunner,
     test_name: str,
     command_args: Dict[str, str],
     response: int,
 ) -> None:
     """Test the trestlebot create-cd command."""
-    # Check that the container image was built successfully
-    # and the mock server is running
-    exit_code, image_name = podman_setup
-    assert exit_code == 0
-
     logger.info(f"Running test: {test_name}")
 
     tmp_repo_str, _ = tmp_repo
@@ -218,9 +198,9 @@ def test_create_cd_e2e(
     _ = setup_for_profile(tmp_repo_path, test_prof, "")
     load_from_json(tmp_repo_path, test_filter_prof, test_filter_prof, Profile)
 
-    command = build_test_command(tmp_repo_str, "create-cd", command_args, image_name)
-    run_response = subprocess.run(command, cwd=tmp_repo_path, capture_output=True)
-    assert run_response.returncode == response
+    command = e2e_runner.build_test_command(tmp_repo_str, "create-cd", command_args)
+    exit_code, _ = e2e_runner.invoke_command(command, tmp_repo_path)
+    assert exit_code == response
 
     # Check that all expected files were created
     if response == SUCCESS_EXIT_CODE:
