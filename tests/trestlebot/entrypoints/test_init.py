21b40432f446323ded883c248feaa064ea1cabd6
Jennifer Power <barnabei.jennifer@gmail.com>
2024-09-16 09:54:58 -0400

    feat: removes provider from init and moves CI templates (#344)
    * fix(init): updates mismatches between init and the initial workflow files
    
    Signed-off-by: Jennifer Power <barnabei.jennifer@gmail.com>
    
    * feat: removes provider from init entrypoint
    
    To simplfy this feature, the provider workflows
    will be available in the repo under TEMPLATES to copy
    manually.
    
    Signed-off-by: Jennifer Power <barnabei.jennifer@gmail.com>
    
    * docs: updates tutorial and corrects workflows
    
    Signed-off-by: Jennifer Power <barnabei.jennifer@gmail.com>
    
    * test: adds back unit tests for directory validation
    
    Signed-off-by: Jennifer Power <barnabei.jennifer@gmail.com>
    
    * test: adds back tests for trestlebot keep file
    
    Signed-off-by: Jennifer Power <barnabei.jennifer@gmail.com>
    
    * fix: addresses PR feedback
    
    Signed-off-by: Jennifer Power <barnabei.jennifer@gmail.com>
    
    * chore(deps): removes extra dependency for importlib
    
    Signed-off-by: Jennifer Power <barnabei.jennifer@gmail.com>
    
    * docs: fixes GitHub tutorial steps
    
    Signed-off-by: Jennifer Power <barnabei.jennifer@gmail.com>
    
    * fix: updates unit tests for trestle API breaking change
    
    Signed-off-by: Jennifer Power <barnabei.jennifer@gmail.com>
    
    ---------
    
    Signed-off-by: Jennifer Power <barnabei.jennifer@gmail.com>

--- tests/trestlebot/entrypoints/test_init.py
+++ tests/trestlebot/entrypoints/test_init.py
@@ -13,7 +13,7 @@ from trestle.common.const import TRESTLE_CONFIG_DIR, TRESTLE_KEEP_FILE
 from trestle.common.file_utils import is_hidden
 
 from tests.testutils import args_dict_to_list, configure_test_logger, setup_for_init
-from trestlebot.const import GITHUB, GITLAB, TRESTLEBOT_CONFIG_DIR, TRESTLEBOT_KEEP_FILE
+from trestlebot.const import TRESTLEBOT_CONFIG_DIR, TRESTLEBOT_KEEP_FILE
 from trestlebot.entrypoints.init import InitEntrypoint
 from trestlebot.entrypoints.init import main as cli_main
 from trestlebot.tasks.authored import types as model_types
@@ -27,7 +27,6 @@ OSCAL_MODEL_COMPDEF = model_types.AuthoredType.COMPDEF.value
 def args_dict() -> Dict[str, str]:
     return {
         "working-dir": ".",
-        "provider": GITHUB,
         "oscal-model": OSCAL_MODEL_COMPDEF,
     }
 
@@ -81,18 +80,30 @@ def test_init_if_not_git_repo(
     )
 
 
+def test_call_trestle_init(tmp_init_dir: str) -> None:
+    """Tests for expected results of calling trestle init"""
+    parser = argparse.ArgumentParser()
+    args = argparse.Namespace(verbose=0, working_dir=tmp_init_dir)
+    InitEntrypoint(parser=parser)._call_trestle_init(args)
+    tmp_dir = pathlib.Path(tmp_init_dir)
+    trestle_dir = tmp_dir / pathlib.Path(TRESTLE_CONFIG_DIR)
+    keep_file = trestle_dir / pathlib.Path(TRESTLE_KEEP_FILE)
+    assert keep_file.exists() is True
+
+
 @patch(
     "trestlebot.entrypoints.log.configure_logger",
     Mock(side_effect=configure_test_logger),
 )
-def test_init_ssp_github(
+def test_init_compdef(
     tmp_init_dir: str, args_dict: Dict[str, str], caplog: pytest.LogCaptureFixture
 ) -> None:
     """Tests for expected init command directories and files"""
     args_dict["working-dir"] = tmp_init_dir
-    args_dict["oscal-model"] = OSCAL_MODEL_SSP
-    args_dict["provider"] = GITHUB
+    args_dict["oscal-model"] = model_types.AuthoredType.COMPDEF.value
+
     setup_for_init(pathlib.Path(tmp_init_dir))
+
     with patch("sys.argv", ["trestlebot", *args_dict_to_list(args_dict)]):
         with pytest.raises(SystemExit, match="0"):
             cli_main()
@@ -103,20 +114,13 @@ def test_init_ssp_github(
     keep_file = trestlebot_dir / pathlib.Path(TRESTLEBOT_KEEP_FILE)
     assert keep_file.exists() is True
 
-    # directories for ssp model should exist
+    # directories for compdef model should exist
+    tmp_dir = pathlib.Path(tmp_init_dir)
     model_dirs = [d.name for d in tmp_dir.iterdir() if not is_hidden(d)]
     expected = InitEntrypoint.MODEL_DIRS[args_dict["oscal-model"]] + [
         InitEntrypoint.MARKDOWN_DIR
     ]
     assert sorted(model_dirs) == sorted(expected)
-
-    # directories for github workflows should exist
-    workflow_dir = tmp_dir.joinpath(".github/workflows")
-    workflow_files = [f.name for f in workflow_dir.iterdir()]
-    expected = InitEntrypoint.PROVIDER_TEMPLATES[args_dict["provider"]][
-        args_dict["oscal-model"]
-    ]
-    assert sorted(workflow_files) == sorted(expected)
 
     # markdown directories should exist
     markdown_dir = tmp_dir.joinpath(InitEntrypoint.MARKDOWN_DIR)
@@ -136,37 +140,13 @@ def test_init_ssp_github(
     "trestlebot.entrypoints.log.configure_logger",
     Mock(side_effect=configure_test_logger),
 )
-def test_init_ssp_gitlab(
-    tmp_init_dir: str, args_dict: Dict[str, str], caplog: pytest.LogCaptureFixture
-) -> None:
-    """Tests for expected logging message"""
-    args_dict["working-dir"] = tmp_init_dir
-    args_dict["oscal-model"] = OSCAL_MODEL_SSP
-    args_dict["provider"] = GITLAB
-    setup_for_init(pathlib.Path(tmp_init_dir))
-    with patch("sys.argv", ["trestlebot", *args_dict_to_list(args_dict)]):
-        with pytest.raises(SystemExit, match="0"):
-            cli_main()
-
-    assert any(
-        record.levelno == logging.WARNING
-        and "GitLab is not yet supported, no provider files will be created."
-        in record.message
-        for record in caplog.records
-    )
-
-
-@patch(
-    "trestlebot.entrypoints.log.configure_logger",
-    Mock(side_effect=configure_test_logger),
-)
-def test_init_compdef_github(
+def test_init_ssp(
     tmp_init_dir: str, args_dict: Dict[str, str], caplog: pytest.LogCaptureFixture
 ) -> None:
     """Tests for expected init command directories and files"""
     args_dict["working-dir"] = tmp_init_dir
-    args_dict["oscal-model"] = model_types.AuthoredType.COMPDEF.value
-    args_dict["provider"] = GITHUB
+    args_dict["oscal-model"] = model_types.AuthoredType.SSP.value
+
     setup_for_init(pathlib.Path(tmp_init_dir))
 
     with patch("sys.argv", ["trestlebot", *args_dict_to_list(args_dict)]):
@@ -181,14 +161,6 @@ def test_init_compdef_github(
     ]
     assert sorted(model_dirs) == sorted(expected)
 
-    # directories for github workflows should exist
-    workflow_dir = tmp_dir.joinpath(".github/workflows")
-    workflow_files = [f.name for f in workflow_dir.iterdir()]
-    expected = InitEntrypoint.PROVIDER_TEMPLATES[args_dict["provider"]][
-        args_dict["oscal-model"]
-    ]
-    assert sorted(workflow_files) == sorted(expected)
-
     # markdown directories should exist
     markdown_dir = tmp_dir.joinpath(InitEntrypoint.MARKDOWN_DIR)
     expected_subdirs = InitEntrypoint.MODEL_DIRS[args_dict["oscal-model"]]
@@ -201,14 +173,3 @@ def test_init_compdef_github(
         in record.message
         for record in caplog.records
     )
-
-
-def test_call_trestle_init(tmp_init_dir: str) -> None:
-    """Tests for expected results of calling trestle init"""
-    parser = argparse.ArgumentParser()
-    args = argparse.Namespace(verbose=0, working_dir=tmp_init_dir)
-    InitEntrypoint(parser=parser)._call_trestle_init(args)
-    tmp_dir = pathlib.Path(tmp_init_dir)
-    trestle_dir = tmp_dir / pathlib.Path(TRESTLE_CONFIG_DIR)
-    keep_file = trestle_dir / pathlib.Path(TRESTLE_KEEP_FILE)
-    assert keep_file.exists() is True
