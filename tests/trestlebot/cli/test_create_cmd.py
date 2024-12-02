742cef932b1c43a8536a8cea2ac6e155ac5eb46c
Hannah Braswell <hbraswel@redhat.com>
2024-12-02 15:53:07 -0500

    feat: unit tests added for create command

--- tests/trestlebot/cli/test_create_cmd.py
+++ tests/trestlebot/cli/test_create_cmd.py
@@ -16,32 +16,123 @@ def test_invalid_create_cmd() -> None:
     assert result.exit_code == 2
 
 
-# def test_create_ssp_cmd(tmp_init_dir: str) -> None:
-#     """Tests successful create ssp command."""
-#
-#     SSP_INDEX_FILE = "test-ssp-index.json"
-#
-#     runner = CliRunner()
-#     result = runner.invoke(
-#         create_cmd,
-# [
-#         "ssp",
-#         "--profile-name",
-#         "repo-path",
-#         tmp_init_dir,
-#         "--ssp-index-file",
-#         SSP_INDEX_FILE,
-#         ],
-#     )
-#     assert result.exit_code == 0
-#
-#     # verify ssp-index.json was created in tmp_init_dir
-#     tmp_dir = pathlib.Path(tmp_init_dir)
-#     ssp_index_file = tmp_dir.joinpath(SSP_INDEX_FILE)
-#     print("SSP INDEX Path", str(ssp_index_file.resolve()))
-#
-#     assert ssp_index_file.exists() is True
+def test_create_ssp_cmd(tmp_init_dir: str) -> None:
+    """Tests successful create ssp command."""
+    SSP_INDEX_FILE = "tester-ssp-index.json"
 
-# verity json file was created in tmp_init_dir/system-security-plans
+    # tmp_dir = pathlib.Path(tmp_init_dir)
+    # ssp_index_file = tmp_dir.joinpath(SSP_INDEX_FILE)
+    # print("SSP INDEX Path", str(ssp_index_file.resolve()))
 
-# verify markdown file(s) were created in tmp_init_dir/markdown/system...
+    runner = CliRunner()
+    result = runner.invoke(
+        create_cmd,
+        [
+            "ssp",
+            "--profile-name",
+            "oscal-profile-name",
+            "--markdown-dir",
+            "markdown",
+            "--ssp-name",
+            "test-name",
+            "--repo-path",
+            tmp_init_dir,
+            "--ssp-index-file",
+            SSP_INDEX_FILE,
+        ],
+    )
+    assert result.exit_code == 0
+
+
+def test_create_compdef_cmd(tmp_init_dir: str) -> None:
+    """Tests successful create compdef command."""
+
+    runner = CliRunner()
+    result = runner.invoke(
+        create_cmd,
+        [
+            "compdef",
+            "--profile-name",
+            "oscal-profile-name",
+            "--compdef-name",
+            "test-name",
+            "--component-title",
+            "title-test",
+            "--component-description",
+            "description-test",
+            "--component-definition-type",
+            "type-test",
+            "repo-path",
+            tmp_init_dir,
+        ],
+    )
+    assert result.exit_code == 2
+
+
+def test_default_ssp_index_file_cmd(tmp_init_dir: str) -> None:
+    """Tests successful default ssp_index.json file creation."""
+
+    runner = CliRunner()
+    result = runner.invoke(
+        create_cmd,
+        [
+            "ssp",
+            "--profile-name",
+            "oscal-profile-name",
+            "--markdown-dir",
+            "markdown",
+            "--ssp-name",
+            "test-name",
+            "--repo-path",
+            tmp_init_dir,
+        ],
+    )
+    assert result.exit_code == 0
+
+
+def test_markdown_files_not_created(tmp_init_dir: str) -> None:
+    SSP_INDEX_TESTER = "ssp-tester.index.json"
+    runner = CliRunner()
+    result = runner.invoke(
+        create_cmd,
+        [
+            "ssp",
+            "--profile-name",
+            "oscal-profile-name",
+            "--ssp-name",
+            "test-name",
+            "repo-path",
+            tmp_init_dir,
+            "--ssp-index-file",
+            SSP_INDEX_TESTER,
+        ],
+    )
+    assert result.exit_code == 2
+
+
+def test_markdown_files_created(tmp_init_dir: str) -> None:
+    SSP_INDEX_TESTER = "ssp-tester.index.json"
+    markdown_file_tester = "/markdown/system-security-plan.md"
+
+    # tmp_dir = pathlib.Path(tmp_init_dir)
+    # markdown_file = tmp_dir.joinpath(markdown_file_tester)
+    # print("Markdown file located at", str(markdown_file.resolve()))
+
+    runner = CliRunner()
+    result = runner.invoke(
+        create_cmd,
+        [
+            "ssp",
+            "--profile-name",
+            "oscal-profile-name",
+            "--markdown-dir",
+            markdown_file_tester,
+            "--ssp-name",
+            "test-name",
+            "--repo-path",
+            tmp_init_dir,
+            "--ssp-index-file",
+            SSP_INDEX_TESTER,
+        ],
+    )
+    assert result.exit_code == 0
