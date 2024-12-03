f9a2bcaedf4c527f406ee61a689b9793e14be401
Hannah Braswell <hbraswel@redhat.com>
2024-12-03 08:40:19 -0500

    fix: docstrings added for create command unit tests

--- tests/trestlebot/cli/test_create_cmd.py
+++ tests/trestlebot/cli/test_create_cmd.py
@@ -1,7 +1,5 @@
 """ Unit test for create commands ssp and cd"""
 
-# import pathlib
-
 from click.testing import CliRunner
 
 from trestlebot.cli.commands.create import create_cmd
@@ -19,10 +17,6 @@ def test_invalid_create_cmd() -> None:
 def test_create_ssp_cmd(tmp_init_dir: str) -> None:
     """Tests successful create ssp command."""
     SSP_INDEX_FILE = "tester-ssp-index.json"
-
-    # tmp_dir = pathlib.Path(tmp_init_dir)
-    # ssp_index_file = tmp_dir.joinpath(SSP_INDEX_FILE)
-    # print("SSP INDEX Path", str(ssp_index_file.resolve()))
 
     runner = CliRunner()
     result = runner.invoke(
@@ -91,7 +85,9 @@ def test_default_ssp_index_file_cmd(tmp_init_dir: str) -> None:
 
 
 def test_markdown_files_not_created(tmp_init_dir: str) -> None:
+    """Tests failure of markdown file creation when not supplied with directory name."""
     SSP_INDEX_TESTER = "ssp-tester.index.json"
+
     runner = CliRunner()
     result = runner.invoke(
         create_cmd,
@@ -111,12 +107,9 @@ def test_markdown_files_not_created(tmp_init_dir: str) -> None:
 
 
 def test_markdown_files_created(tmp_init_dir: str) -> None:
+    """Tests successful creation of markdown files."""
     SSP_INDEX_TESTER = "ssp-tester.index.json"
     markdown_file_tester = "/markdown/system-security-plan.md"
-
-    # tmp_dir = pathlib.Path(tmp_init_dir)
-    # markdown_file = tmp_dir.joinpath(markdown_file_tester)
-    # print("Markdown file located at", str(markdown_file.resolve()))
 
     runner = CliRunner()
     result = runner.invoke(
