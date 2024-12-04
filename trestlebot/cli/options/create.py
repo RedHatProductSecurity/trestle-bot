be89e66f4beda8b517d5c3134f9c24904f654f05
Hannah Braswell <hbraswel@redhat.com>
2024-12-03 22:48:27 -0500

    fix: update for clarity on profile name for trestle workspace

--- trestlebot/cli/options/create.py
+++ trestlebot/cli/options/create.py
@@ -21,7 +21,7 @@ def common_create_options(f: F) -> F:
 
     @click.option(
         "--profile-name",
-        prompt="Name of trestle workspace to include",
+        prompt="Name of profile in trestle workspace to include",
         help="Name of profile in trestle workspace to include.",
     )
     @click.option(
