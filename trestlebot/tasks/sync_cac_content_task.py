caaa44d0770b92b834d55fed1ee7204f7cd67be6
Qingmin Duanmu <qduanmu@redhat.com>
2025-01-13 14:48:18 +0800

    feat: add cac content rules transformation

--- trestlebot/tasks/sync_cac_content_task.py
+++ trestlebot/tasks/sync_cac_content_task.py
@@ -1,35 +1,135 @@
 # SPDX-License-Identifier: Apache-2.0
-# Copyright (c) 2023 Red Hat, Inc.
+# Copyright (c) 2024 Red Hat, Inc.
+
+"""Trestle Bot Sync CaC Content Tasks"""
+
+import datetime
+import logging
+import os
+import pathlib
+from typing import List
+
+# from ssg.products import get_all
+from ssg.profiles import get_profiles_from_products
+from trestle.common import const as trestle_const
+from trestle.common.list_utils import none_if_empty
+from trestle.core.generators import generate_sample_model
+from trestle.oscal.common import Property
+from trestle.oscal.component import ComponentDefinition, DefinedComponent
+
+from trestlebot import const
+from trestlebot.tasks.authored.base_authored import AuthoredObjectBase
+from trestlebot.tasks.base_task import TaskBase
+from trestlebot.transformers.cac_transformer import (
+    RuleInfo,
+    RulesTransformer,
+    get_component_info,
+)
 
 
-"""Trestle Bot Rule Transform Tasks"""
-
-from typing import Optional
-
-import trestlebot.const as const
-from trestlebot.tasks.base_task import ModelFilter, TaskBase
+logger = logging.getLogger(__name__)
 
 
 class SyncCacContentTask(TaskBase):
-    """
-    Transform rules into OSCAL content.
-    """
+    """Sync CaC content to OSCAL component definition task."""
 
     def __init__(
         self,
-        working_dir: str,
-        model_filter: Optional[ModelFilter] = None,
+        product: str,
+        cac_profile: str,
+        cac_content_root: str,
+        compdef_type: str,
+        oscal_profile: str,
+        authored_object: AuthoredObjectBase,
     ) -> None:
-        """
-        Initialize transform task.
+        """Initialize CaC content sync task."""
+        self.product: str = product
+        self.cac_profile: str = cac_profile
+        self.cac_content_root: str = cac_content_root
+        self.compdef_type: str = compdef_type
+        self.rules: List[str] = []
 
-        Args:
-            working_dir: Working directory to complete operations in
-            model_filter: Optional filter to apply to the task to include or exclude models
-            from processing.
-        """
-        super().__init__(working_dir, model_filter)
+        self._authored_object = authored_object
+        working_dir = self._authored_object.get_trestle_root()
+        super().__init__(working_dir, None)
+
+    def _collect_rules(self) -> None:
+        """Collect all rules from the product profile."""
+        profiles = get_profiles_from_products(self.cac_content_root, [self.product])
+        cac_profile_id = os.path.basename(self.cac_profile).split(".profile")[0]
+        for profile in profiles:
+            if profile.profile_id == cac_profile_id:
+                self.rules = profile.rules
+                break
+
+    def _get_rules_properties(self) -> List[Property]:
+        """Create all of the top-level component properties for rules."""
+        rules_transformer = RulesTransformer(
+            self.cac_content_root,
+            self.product,
+        )
+        rules_transformer.add_rules(self.rules)
+        rules: List[RuleInfo] = rules_transformer.get_all_rules()
+        all_rule_properties: List[Property] = rules_transformer.transform(rules)
+        return all_rule_properties
+
+    def _create_or_update_compdef(self, compdef_type: str = "service") -> None:
+        """Create a component definition for specified product."""
+        component_definition = generate_sample_model(ComponentDefinition)
+        component_definition.metadata.title = f"Component definition for {self.product}"
+        component_definition.metadata.version = "1.0"
+        component_definition.components = list()
+
+        oscal_component = generate_sample_model(DefinedComponent)
+        product_name, full_name = get_component_info(
+            self.product, self.cac_content_root
+        )
+        oscal_component.title = product_name
+        oscal_component.type = compdef_type
+        oscal_component.description = full_name
+        all_rule_properties = self._get_rules_properties()
+        oscal_component.props = none_if_empty(all_rule_properties)
+
+        repo_path = pathlib.Path(self.working_dir)
+        cd_dir = repo_path.joinpath(f"{trestle_const.MODEL_DIR_COMPDEF}/{self.product}")
+        cd_json = cd_dir / "component-definition.json"
+        if cd_json.exists():
+            logger.info(f"The component definition for {self.product} exists.")
+            compdef = ComponentDefinition.oscal_read(cd_json)
+            updated = False
+            for index, component in enumerate(compdef.components):
+                if component.title == oscal_component.title:
+                    if component.props != oscal_component.props:
+                        compdef.components[index].props = oscal_component.props
+                        updated = True
+                        break
+            if updated:
+                logger.info(f"Update component definition: {cd_json}")
+                compdef.metadata.version = str(
+                    "{:.1f}".format(float(compdef.metadata.version) + 0.1)
+                )
+                compdef.metadata.last_modified = (
+                    datetime.datetime.now(datetime.timezone.utc)
+                    .replace(microsecond=0)
+                    .isoformat()
+                )
+                compdef.oscal_write(cd_json)
+        else:
+            logger.info(f"Creating component definition for product {self.product}")
+            cd_dir.mkdir(exist_ok=True, parents=True)
+            cd_json = cd_dir / "component-definition.json"
+            component_definition.components.append(oscal_component)
+            component_definition.oscal_write(cd_json)
 
     def execute(self) -> int:
-        """Execute task"""
+        """Execute task to create or update product component definition."""
+        # Check the product existence in CaC content.
+        # Comment below due to the hardcoded product_directories in get_all,
+        # all_products = list(set().union(*get_all(self.cac_content_root)))
+        # if self.product not in all_products:
+        #     raise TaskException(f"Product {self.product} does not exist.")
+        # Collect all selected rules in product profile
+        self._collect_rules()
+        # Create or update product component definition
+        self._create_or_update_compdef()
         return const.SUCCESS_EXIT_CODE
--- trestlebot/transformers/cac_transformer.py
+++ trestlebot/transformers/cac_transformer.py
@@ -1,22 +1,33 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (c) 2024 Red Hat, Inc.
 
-import datetime
-import json
-from pathlib import Path
-from typing import Tuple
+"""Transform rules from existing Compliance as Code locations into OSCAL properties."""
 
-from ssg.products import load_product_yaml, product_yaml_path
+import logging
+import os
+import re
+from html.parser import HTMLParser
+from typing import Dict, List, Optional, Tuple
+
+import ssg.build_yaml
+import ssg.products
+import ssg.rules
+from trestle.common.const import TRESTLE_GENERIC_NS
+from trestle.core.generators import generate_sample_model
+from trestle.oscal.common import Property
+from trestle.tasks.csv_to_oscal_cd import RULE_DESCRIPTION, RULE_ID, _RuleSetIdMgr
+
+
+logger = logging.getLogger(__name__)
+
+TRESTLE_CD_NS = f"{TRESTLE_GENERIC_NS}/cd"
 
 
 def get_component_info(product_name: str, cac_path: str) -> Tuple[str, str]:
     """Get the product name from product yml file via the SSG library."""
     if product_name and cac_path:
-        # Get the product yaml file path
-        product_yml_path = product_yaml_path(cac_path, product_name)
-        # Load the product data
-        product = load_product_yaml(product_yml_path)
-        # Return product name from product yml file
+        product_yml_path = ssg.products.product_yaml_path(cac_path, product_name)
+        product = ssg.products.load_product_yaml(product_yml_path)
         component_title = product._primary_data.get("product")
         component_description = product._primary_data.get("full_name")
         return (component_title, component_description)
@@ -24,15 +35,173 @@ def get_component_info(product_name: str, cac_path: str) -> Tuple[str, str]:
         raise ValueError("component_title is empty or None")
 
 
-def update_component_definition(compdef_file: Path) -> None:
-    # Update the component definition version and modify time
-    with open(compdef_file, "r", encoding="utf-8") as f:
-        data = json.load(f)
-    current_version = data["component-definition"]["metadata"]["version"]
-    data["component-definition"]["metadata"]["version"] = str(
-        "{:.1f}".format(float(current_version) + 0.1)
-    )
-    current_time = datetime.datetime.now().isoformat()
-    data["component-definition"]["metadata"]["last-modified"] = current_time
-    with open(compdef_file, "w", encoding="utf-8") as f:
-        json.dump(data, f, ensure_ascii=False, indent=2)
+def add_prop(name: str, value: str, remarks: Optional[str] = None) -> Property:
+    """Add a property to a set of rule properties."""
+    prop = generate_sample_model(Property)
+    prop.name = name
+    prop.value = value
+    if remarks:
+        prop.remarks = remarks
+    prop.ns = TRESTLE_CD_NS  # type: ignore
+    return prop
+
+
+def get_benchmark_root(root: str, product: str) -> str:
+    """Get the benchmark root."""
+    product_yaml_path = ssg.products.product_yaml_path(root, product)
+    product_yaml = ssg.products.load_product_yaml(product_yaml_path)
+    product_dir = product_yaml.get("product_dir")
+    benchmark_root = os.path.join(product_dir, product_yaml.get("benchmark_root"))
+    return benchmark_root
+
+
+class RuleInfo:
+    """Stores rule information."""
+
+    def __init__(self, rule_id: str, rule_dir: str) -> None:
+        """Initialize."""
+        self._id = rule_id
+        self._description = ""
+        self._rule_dir = rule_dir
+
+    @property
+    def id(self) -> str:
+        """Get the id."""
+        return self._id
+
+    @property
+    def description(self) -> str:
+        """Get the description."""
+        return self._description
+
+    @property
+    def rule_dir(self) -> str:
+        """Get the rule directory."""
+        return self._rule_dir
+
+    def add_description(self, value: str) -> None:
+        """Add a rule description."""
+        self._description = value
+
+
+class RulesTransformer:
+    """Transforms rules into properties for creating component definitions."""
+
+    def __init__(
+        self,
+        root: str,
+        product: str,
+    ) -> None:
+        """Initialize."""
+        self.root = root
+        self.product = product
+
+        benchmark_root = get_benchmark_root(root, self.product)
+        self.rules_dirs_for_product: Dict[str, str] = dict()
+        for dir_path in ssg.rules.find_rule_dirs_in_paths([benchmark_root]):
+            rule_id = ssg.rules.get_rule_dir_id(dir_path)
+            self.rules_dirs_for_product[rule_id] = dir_path
+
+        self._rules_by_id: Dict[str, RuleInfo] = dict()
+
+    def add_rules(self, rules: List[str]) -> None:
+        """
+        Load a set of rules into rule objects based on ids and
+        add them to the rules_by_id dictionary.
+        Args:
+            rules: A list of rule ids.
+        Notes: This attempt to load all rules and will raise an error if any fail.
+        """
+        rule_errors: List[str] = list()
+
+        for rule_id in rules:
+            error = self._add_rule(rule_id)
+            if error:
+                rule_errors.append(error)
+
+        if len(rule_errors) > 0:
+            raise RuntimeError(
+                f"Error loading rules: \
+                    \n{', '.join(rule_errors)}"
+            )
+
+    def _add_rule(self, rule_id: str) -> Optional[str]:
+        """Add a single rule to the rules_by_id dictionary."""
+        try:
+            if rule_id not in self._rules_by_id:
+                rule_obj = self._new_rule_obj(rule_id)
+                self._load_rule_yaml(rule_obj)
+                self._rules_by_id[rule_id] = rule_obj
+        except ValueError as e:
+            return f"Could not find rule {rule_id}: {e}"
+        except FileNotFoundError as e:
+            return f"Could not load rule {rule_id}: {e}"
+        return None
+
+    def _new_rule_obj(self, rule_id: str) -> RuleInfo:
+        """Create a new rule object."""
+        rule_dir = self._from_product_dir(rule_id)
+        if not rule_dir:
+            raise ValueError(f"Could not find rule {rule_id} directory.")
+            # Or skip here if not found?
+        rule_obj = RuleInfo(rule_id, rule_dir)
+        return rule_obj
+
+    def _from_product_dir(self, rule_id: str) -> Optional[str]:
+        """Locate the rule dir in the product directory."""
+        if rule_id not in self.rules_dirs_for_product:
+            return None
+        return self.rules_dirs_for_product.get(rule_id)
+
+    def _load_rule_yaml(self, rule_obj: RuleInfo) -> None:
+        """
+        Update the rule object with the rule yaml data.
+
+        Args:
+            rule_obj: The rule object where collection rule data is stored.
+        """
+        # The function will raise error: FileNotFoundError
+        # which is caused by loading jinja macros, and this
+        # needs to be fixed by ssg. So comment blow temporarily.
+        # rule_file = ssg.rules.get_rule_dir_yaml(rule_obj.rule_dir)
+        # rule_yaml = ssg.build_yaml.Rule.from_yaml(rule_file)
+        # rule_yaml.normalize(self.product)
+        # description = self._clean_rule_description(rule_yaml.description)
+        # rule_obj.add_description(description)
+        rule_obj.add_description("Pending update")
+
+    @staticmethod
+    def _clean_rule_description(description: str) -> str:
+        """Clean the rule description."""
+        parser = HTMLParser()
+        parser.handle_data(description)
+        cleaned_description = description.replace("\n", " ").strip()
+        cleaned_description = re.sub(" +", " ", cleaned_description)
+        return cleaned_description
+
+    def _get_rule_properties(self, ruleset: str, rule_obj: RuleInfo) -> List[Property]:
+        """Get a set of rule properties for a rule object."""
+        rule_properties: List[Property] = list()
+        # Add rule properties for the rule set
+        rule_properties.append(add_prop(RULE_ID, rule_obj.id, ruleset))
+        rule_properties.append(
+            add_prop(RULE_DESCRIPTION, rule_obj.description, ruleset)
+        )
+        return rule_properties
+
+    def get_all_rules(self) -> List[RuleInfo]:
+        """Get all rules that have been loaded"""
+        return list(self._rules_by_id.values())
+
+    def transform(self, rule_objs: List[RuleInfo]) -> List[Property]:
+        """Get the rules properties for a set of rule ids."""
+        rule_properties: List[Property] = list()
+
+        start_val = -1
+        for i, rule_obj in enumerate(rule_objs):
+            rule_set_mgr = _RuleSetIdMgr(start_val + i, len(rule_objs))
+            rule_set_props = self._get_rule_properties(
+                rule_set_mgr.get_next_rule_set_id(), rule_obj
+            )
+            rule_properties.extend(rule_set_props)
+        return rule_properties
