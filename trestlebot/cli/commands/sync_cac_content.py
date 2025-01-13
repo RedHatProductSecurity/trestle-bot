129fd5f8efc13e990e7fba08f2fd1e6662f7219b
Hannah Braswell <hbraswel@redhat.com>
2025-01-13 18:54:18 -0500

    feat: adding unit test for sync_cac_content_profile subcommand
     The initial setup for the interaction of the subcommand interacting with the task. The unit tests included testing for invalid subcommand for the `sync_cac_content_cmd`, and missing required click options from the CLI.

--- trestlebot/cli/commands/sync_cac_content.py
+++ trestlebot/cli/commands/sync_cac_content.py
@@ -10,7 +10,9 @@ import click
 from trestlebot.cli.options.common import common_options, git_options, handle_exceptions
 from trestlebot.cli.utils import run_bot
 from trestlebot.tasks.authored.compdef import AuthoredComponentDefinition
+from trestlebot.tasks.authored.profile import AuthoredProfile
 from trestlebot.tasks.base_task import TaskBase
+from trestlebot.tasks.sync_cac_content_profile_task import SyncCacContentProfileTask
 
 # from trestlebot.tasks.sync_cac_content_profile_task import SyncCacContentProfileTask
 from trestlebot.tasks.sync_cac_content_task import SyncCacContentTask
@@ -91,32 +93,43 @@ def sync_cac_content_cmd(ctx: click.Context, **kwargs: Any) -> None:
     run_bot(pre_tasks, kwargs)
 
 
-@sync_cac_content_cmd.command(name="oscal_profile_cmd", help="Authoring Oscal Profile")
+@sync_cac_content_cmd.command(
+    name="oscal-profile", help="Authoring Oscal Profiles with synced CaC content."
+)
+@click.pass_context
+@common_options
+@git_options
 @click.option(
     "--cac-content-root",
-    help="Root of the CaC content project.",
     required=True,
+    help="Root of the CaC content project.",
 )
 @click.option(
     "--product",
     type=str,
     required=True,
-    help="Product name for getting associated control files.",
+    help="Product to build OSCAL Profile for.",
 )
 @click.option(
-    "--catalog",
+    "--oscal-catalog",
     type=str,
     required=True,
-    help="Catalog leveraged for import of control file data.",
+    help="Main catalog href, or name of the catalog in trestle workspace.",
 )
-@click.option("--control-file", type=str, required=True, help="Name of OSCAL Profile.")
+@click.option(
+    "--policy-id",
+    type=str,
+    required=True,
+    help="Policy id for source control file.",
+)
 @click.option(
     "--filter-by-level",
-    type=str,
+    type=List[str],
     required=False,
     help="Optionally produce OSCAL Profiles by filtered baseline level.",
 )
-def oscal_profile_cmd(
+@handle_exceptions
+def sync_cac_content_profile_cmd(
     ctx: click.Context,
     **kwargs: Any,
 ) -> None:
@@ -125,24 +138,33 @@ def oscal_profile_cmd(
     # User will input control file name to begin authoring OSCAL Profiles
     # If user indicates level, a profile specific to indicated level will be produced
     # If no level associated with control file, task will create single profile with all controls
-    # pre_tasks: List[TaskBase] = []
-    #
-    # cac_content_root = kwargs["cac_content_root"]
-    # product = kwargs["product"]
-    # catalog = kwargs["catalog"]
-    # control_file = kwargs["control_file"]
-    # filter_by_level = kwargs.get("filter_by_level", None)
-    #
-    #
-    # sync_cac_content_profile_task: SyncCacContentProfileTask = (
-    #     SyncCacContentProfileTask(
-    #         working_dir=cac_content_root,
-    #         control_file=control_file,
-    #         filter_by_level=filter_by_level,
-    #     )
-    # )
-    # logger.debug(f"No levels included in control file.")
-    #
-    # pre_tasks.append(sync_cac_content_profile_task)
-    # run_bot(pre_tasks, kwargs)
-    pass
+
+    pre_tasks: List[TaskBase] = []
+
+    working_dir = kwargs["repo_path"]
+    cac_content_root = kwargs["cac_content_root"]
+    product = kwargs["product"]
+    oscal_catalog = kwargs["oscal_catalog"]
+    policy_id = kwargs["policy_id"]
+    filter_by_level = kwargs.get("filter_by_level", list())
+
+    authored_profile: AuthoredProfile = AuthoredProfile(trestle_root=working_dir)
+
+    sync_cac_content_profile_task: SyncCacContentProfileTask = (
+        SyncCacContentProfileTask(
+            cac_content_root=cac_content_root,
+            product=product,
+            oscal_catalog=oscal_catalog,
+            policy_id=policy_id,
+            filter_by_level=filter_by_level,
+            authored_profile=authored_profile,
+        )
+    )
+    logger.debug(f"No levels included in control file.")
+
+    pre_tasks.append(sync_cac_content_profile_task)
+    run_bot(pre_tasks, kwargs)
+
+
+if __name__ == "__main__":
+    sync_cac_content_profile_cmd()
--- trestlebot/tasks/sync_cac_content_profile_task.py
+++ trestlebot/tasks/sync_cac_content_profile_task.py
@@ -5,18 +5,16 @@ import logging
 import os
 from typing import Any, Dict, List
 
-from ssg.build_profile import make_name_to_profile_mapping
 from ssg.controls import ControlsManager
-from ssg.entities.profile import ProfileWithInlinePolicies
 from ssg.products import (
     get_profile_files_from_root,
     load_product_yaml,
     product_yaml_path,
 )
 
-from trestlebot.tasks.authored.base_authored import AuthoredObjectBase
-from trestlebot.tasks.base_task import TaskBase
-from trestlebot.tasks.sync_cac_content_task import get_env_yaml
+from trestlebot import const
+from trestlebot.tasks.authored.profile import AuthoredProfile
+from trestlebot.tasks.base_task import TaskBase, TaskException
 
 
 logger = logging.getLogger(__name__)
@@ -27,19 +25,21 @@ class SyncCacContentProfileTask(TaskBase):
         self,
         cac_content_root: str,
         product: str,
-        control_file: str,
+        oscal_catalog: str,
+        policy_id: str,
         filter_by_level: List[str],
         authored_profile: AuthoredProfile,
     ):
         self.cac_content_root = cac_content_root
         self.product = product
-        self.control_file = control_file
+        self.oscal_catalog = oscal_catalog
+        self.policy_id = policy_id
         self.filter_by_level = filter_by_level
         self.authored_profile = authored_profile
-        working_dir = self._authored_profile.get_trestle_root()
-        super().__init__(working_dir=trestle_root, model_filter=None)
+        working_dir = self.authored_profile.get_trestle_root()
+        super().__init__(working_dir=working_dir, model_filter=None)
 
-    def get_control_ids_by_level(self, control_file: str, filter_by_level: str) -> None:
+    def get_control_ids_by_level(self, policy_id: str, filter_by_level: str) -> None:
         """
         Collecting control file product data
         """
@@ -52,47 +52,59 @@ class SyncCacContentProfileTask(TaskBase):
         )
 
         control_manager.load()
-        # control_manager is an instance of the ControlsManager() and it is using
-        # cac_content_root/controls to evaluate
         # accessing control file within content/controls
         # the instance can use the methods within the ControlsManager() class
-        # Use for parsing and organizing by level
-        default_levels = control_manager.get_all_controls()
-        extract_policy = control_manager._get_policy(control_file)
 
+        # TODO Ask Marcus to address use of get_policy in PR notes
+        default_levels = control_manager.get_all_controls(policy_id)
+        extract_policy = control_manager._get_policy(policy_id)
+        logger.debug(
+            f"Organizing controls based on {filter_by_level}. If no level is specified, all controls will be organized."
+        )
         if not filter_by_level:
-            all_controls = control_manager.get_all_controls(control_file)
+            all_controls = control_manager.get_all_controls(policy_id)
             self.create_oscal_profile(all_controls)
+            logger.debug(
+                f"No level indicated. Sorting based on {policy_id} default levels."
+            )
         else:
             for level in filter_by_level:
                 eligible_controls = control_manager.get_all_controls_of_level(level)
                 self.create_oscal_profile(eligible_controls)
-            # make oscal profile
-            logger.debug(
-                f"Handling levels: {handling_levels} and changing filename to include {filter_by_level}"
-            )
-            # if filter_by_profile = "high" filter_by_profile.upper()
-            # when making new json -> profile-HIGH.json
+                # make oscal profile
+                logger.debug(f"Creating oscal profile using {eligible_controls}.")
+                # if filter_by_profile = "high" filter_by_profile.upper()
+                # when making new json -> profile-HIGH.json
 
     def create_oscal_profile(
         self,
         import_path: str,
         controls: List[str],
         name_update: str,
-    ):
+    ) -> None:
         # Step 1: If filter by level returns eligible controls, create OSCAL profile with suffix change based on level
         # Step 2: Fill in with control id, loading from eligible controls and all controls
-        self.import_path = kwargs["catalog"]
+        self.import_path = import_path
+        self.controls = controls
         # catalog is import_path
-        name_update = f"{control_file}-{level}.json"
+        self.name_update = name_update
+        name_update = f"{self.policy_id}-{self.filter_by_level}.json"
+        # If the import_path is not valid then create new default (based on tasks/authored/profile.py)
+        # Otherwise the existing copy is held as a deep copy and will be accessible even if changing profile inputs
+        # Checks for importing model types (catalog, baseline)
+        # AuthoredProfile will update based on existing_import
+        # If the models aren't equivalent based on the deep copy of the existing profile
+        # Then modelUtils will update profile element that was recently updated and write to the profile_path
         self.authored_profile.create_new_default(controls, name_update)
 
     def execute(self) -> int:
         # calling to get_control_ids _by_level and checking for valid control file name
         try:
-            get_control_ids_by_level(
-                control_file=self.control_file, filter_by_level=self.filter_by_level
+            self.get_control_ids_by_level(
+                policy_id=self.policy_id, filter_by_level=self.filter_by_level
             )
-        except KeyError:
-            raise f"The control file {self.control_file} does not exist."
+        except KeyError as e:
+            raise TaskException(
+                f"The control file associated with {self.policy_id} does not exist."
+            ) from e
         return const.SUCCESS_EXIT_CODE
