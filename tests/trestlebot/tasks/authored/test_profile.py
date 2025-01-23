7303eb052530aadb89c3daf5017ed4fdc3591d7f
Hannah Braswell <hbraswel@redhat.com>
2025-01-23 14:27:50 +0100

    feat: sync_cac_content command and task CPLYTM-437 for OSCAL Profiles This commit specifically addresses CPLYTM-437 - creating a base level skeleton to match control ids with profiles. This commit includes the initial setup work for the SyncCacContentProfile task and the `sync_cac_content_profile` CLI command. The merged commits from trestle-bot complytime:main haven't been altered, but are included from rebasing commits. The original merged commit messages from complytime:main of trestle-bot are listed below for reference.

--- tests/trestlebot/tasks/authored/test_profile.py
+++ tests/trestlebot/tasks/authored/test_profile.py
@@ -7,12 +7,13 @@
 import os
 import pathlib
 
+import pytest
 from trestle.common.model_utils import ModelUtils
 from trestle.core.models.file_content_type import FileContentType
 from trestle.oscal.profile import CombinationMethodValidValues, Profile
 
 from tests import testutils
-from trestlebot.tasks.authored.profile import AuthoredProfile
+from trestlebot.tasks.authored.profile import AuthoredProfile, CatalogControlResolver
 
 
 test_prof = "simplified_nist_profile"
@@ -39,9 +40,23 @@ def test_create_new_default(tmp_trestle_dir: str) -> None:
     assert prof.merge.combine is not None
     assert prof.merge.combine.method is CombinationMethodValidValues.merge
 
-    assert prof.imports is not None
+    assert len(prof.imports) == 1
     assert prof.imports[0].include_all is not None
     assert cat_path in prof.imports[0].href
+
+    authored_prof.create_new_default(cat_path, test_prof, ["ac-1", "ac-2"])
+    prof, _ = ModelUtils.load_model_for_class(
+        trestle_root, test_prof, Profile, FileContentType.JSON
+    )
+    assert prof.merge is not None
+    assert prof.merge.combine is not None
+    assert prof.merge.combine.method is CombinationMethodValidValues.merge
+
+    assert len(prof.imports) == 1
+    assert prof.imports[0].include_all is None
+    assert cat_path in prof.imports[0].href
+    assert prof.imports[0].include_controls is not None
+    assert len(prof.imports[0].include_controls[0].with_ids) == 2
 
 
 def test_create_new_default_existing(tmp_trestle_dir: str) -> None:
@@ -67,3 +82,50 @@ def test_create_new_default_existing(tmp_trestle_dir: str) -> None:
     assert prof.imports is not None
     assert prof.imports[0].include_all is not None
     assert cat_path in prof.imports[0].href
+
+
+def test_create_or_update(tmp_trestle_dir: str) -> None:
+    """Test updating a profile in place."""
+    # Prepare the workspace
+    trestle_root = pathlib.Path(tmp_trestle_dir)
+    _ = testutils.setup_for_profile(trestle_root, test_prof, "")
+
+    authored_prof = AuthoredProfile(tmp_trestle_dir)
+
+    cat_path = os.path.join("catalogs", test_cat, "catalog.json")
+
+    updated = authored_prof.create_or_update(cat_path, test_prof, ["ac-1", "ac-2"])
+    assert updated
+
+    prof, _ = ModelUtils.load_model_for_class(
+        trestle_root, test_prof, Profile, FileContentType.JSON
+    )
+
+    assert prof.imports is not None
+    assert prof.imports[0].include_all is None
+    assert cat_path in prof.imports[0].href
+    assert prof.imports[0].include_controls is not None
+    assert len(prof.imports[0].include_controls[0].with_ids) == 2
+
+    updated = authored_prof.create_or_update(cat_path, test_prof, ["ac-1", "ac-2"])
+    assert not updated
+
+
+@pytest.mark.parametrize(
+    "input, response",
+    [
+        ("AC-1", "ac-1"),
+        ("AC-2(2)", "ac-2.2"),
+        ("ac-1_smt.a", "ac-1_smt.a"),
+        ("AC-200", None),
+    ],
+)
+def test_control_resolver(tmp_trestle_dir: str, input: str, response: str) -> None:
+    "Test the CatalogControlResolver class."
+    trestle_root = pathlib.Path(tmp_trestle_dir)
+    _ = testutils.setup_for_catalog(trestle_root, test_cat, test_cat)
+    c2l = CatalogControlResolver(trestle_root)
+    cat_path = trestle_root.joinpath("catalogs", test_cat, "catalog.json")
+    c2l.load(cat_path)
+    result_id = c2l.get_id(input)
+    assert result_id == response
